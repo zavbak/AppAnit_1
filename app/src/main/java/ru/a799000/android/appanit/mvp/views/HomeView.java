dc96b17b9c7e3a63ed984190d7385ef6173220d1
zavbak <799000@mail.ru>
2017-05-11 21:33:38 +1000

    @StateStrategyType(OneExecutionStateStrategy.class) решил проблему с Fragmentom вернее помогли

--- app/src/main/java/ru/a799000/android/appanit/mvp/views/HomeView.java
+++ app/src/main/java/ru/a799000/android/appanit/mvp/views/HomeView.java
@@ -2,9 +2,11 @@ package ru.a799000.android.appanit.mvp.views;
 
 import com.arellomobile.mvp.MvpView;
 import com.arellomobile.mvp.viewstate.strategy.AddToEndSingleStrategy;
+import com.arellomobile.mvp.viewstate.strategy.OneExecutionStateStrategy;
 import com.arellomobile.mvp.viewstate.strategy.StateStrategyType;
 
 @StateStrategyType(AddToEndSingleStrategy.class)
 public interface HomeView extends MvpView {
+    @StateStrategyType(OneExecutionStateStrategy.class)
     void showList();
 }
